4f55b9cac09365a250b09152cbe09234dbce2ab3
Your Name <you@example.com>
2024-12-02 10:09:29 +0000

    a tentar ligar à bd

--- assets/php/login.php
+++ assets/php/login.php
@@ -23,13 +23,26 @@ $sql = "SELECT * FROM utilizadores WHERE nome_completo = '$nome_completo'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "Utilizador não encontrado. Você pode registar-se <a href='../logins/registo com validacao.html'>aqui</a>.";
     $row = $result->fetch_assoc();
     
     // Verificar a senha
-    if (password_verify($senha, $row['senha'])) {
+    if (password_verify($senha, $row['password'])) {
         // Armazenar o nome do utilizador na sessão
         $_SESSION['nome_completo'] = $row['nome_completo'];
-        
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['admin'] = $row['admin'];
+
+        // Verificar o código secreto
+        if ($_POST['codigo_secreto'] == '1234'){
+            $_SESSION['admin'] = 1;
+        }
+
+        // Registar a atividade
+        $atividade = isset($_POST['oquefazer']) ? implode(", " $_POST['oquefazer']) : 'Nenhuma';
+        $sql = "INSERT INTO atividades (nome_completo, atividade) VALUES ('{$_SESSION['user_id']}', '$atividade')";
+        $conn->query($sql);
+
         // Redirecionar para index.html
         header("Location: index.html");
         exit();
