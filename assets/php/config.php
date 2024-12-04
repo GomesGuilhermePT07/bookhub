a018a8fc5c39d33c015c8d56ca25b20c061cffae
Your Name <you@example.com>
2024-12-04 21:58:45 +0000

    commit

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,11 +1,12 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=bookhub";
-$username = "root";
-$password = "usbw";
+
+$dsn = "mysql:host=localhost;dbname=myfirstdatabase";
+$dbusername = "root";
+$dbpassword = "usbw";
 
 try {
-    $pdo = new PDO($dsn, $username, $password);
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
-    die("Connection failed: " . $e->getMessage();)
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
