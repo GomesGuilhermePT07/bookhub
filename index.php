9f8e5a01ee8f355a785f5a11278bcc02fd04df25
Your Name <you@example.com>
2025-02-18 19:20:10 +0000

    add

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-session_start();
+// session_start();
+
+    if(isset($_POST['submit'])){
+        print_r($_POST['isbn']);
+        print_r($_POST['title']);
+        print_r($_POST['edition']);
+        print_r($_POST['author']);
+        print_r($_POST['numero_paginas']);
+        print_r($_POST['quantity']);
+    }
+
+
 ?>
 
 <!DOCTYPE html>
