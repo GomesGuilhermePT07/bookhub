570444ad6e51d99fd3149df5b85a3695b9e41cc3
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-02-24 09:05:25 +0000

    add

--- index_user.php
+++ index_user.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-PT">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">~
+    <link rel="stylesheet" href="../ModuloProjeto/assets/css/index_style.css">
     <title>BOOKhub</title>
 </head>
 <body>
