86b5f3b9ecfde8d2f47a30892bfd8485e5ed7686
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-02-27 09:04:51 +0000

    2

--- index_user.php
+++ index_user.php
@@ -57,6 +57,6 @@
         <!-- <p>&copy; 2025 BOOKhub. Todos os direitos reservados.</p>  -->
     </footer>
 
-    <!-- <p>isto é um teste</p> -->
+    <!-- <p>esta linha é um teste</p> -->
 </body>
 </html>
