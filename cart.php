8816172a8c31f7b1bfc64b1811ffc63a0c95590a
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-06 09:12:08 +0000

    add

--- cart.php
+++ cart.php
@@ -27,42 +27,39 @@ require_once 'assets/php/check_login.php'; // Verifica se o utilizador está log
                     <thead>
                         <tr>
                             <th>Livro</th>
-                            <th>Preço</th>
+                            <!-- <th>Preço</th> -->
                             <th>Quantidade</th>
-                            <th>Total</th>
+                            <!-- <th>Total</th> -->
                             <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                            $total = 0;
-                            foreach($_SESSION['cart'] as $id => $item):
-                            $subtotal = $item['price'] * $item['quantity'];
-                            $total += $subtotal;
+                            // $total = 0;
+                            foreach($_SESSION['cart'] as $isbn => $item):
+                            // $subtotal = $item['price'] * $item['quantity'];
+                            // $total += $subtotal;
                         ?>
                     <tr>
                         <td><?= $item['title'] ?></td>
-                        <td>€<?= number_format($item['price'], 2) ?></td>
+                        
                         <td>
                             <form action="./assets/php/update_cart.php" method="POST">
                                 <input type="number" name="quantity" value="<?= $item['quantity'] ?>" min="1">
-                                <input type="hidden" name="book_id" value="<?= $id ?>">
+                                <input type="hidden" name="isbn" value="<?= $isbn ?>">
                                 <button type="submit">Atualizar</button>
                             </form>
                         </td>
-                        <td>€<?= number_format($subtotal, 2) ?></td>
+                        
                         <td>
-                            <a href="./assets/php/remove_from_cart.php?id=<?= $id ?>" class="btn-remove">Remover</a>
+                            <a href="./assets/php/remove_from_cart.php?isbn=<?= $isbn ?>" class="btn-remove">Remover</a>
                         </td>
                     </tr>
                     <?php endforeach; ?>
                     </tbody>
                 </table>
 
-                <div class="cart-total">
-                    <h3>Total do Carrinho: €<?= number_format($total, 2) ?></h3>
-                    <a href="checkout.php" class="btn-checkout">Finalizar Compra</a>
-                </div>
+                 
             </div>
             <?php else: ?>
             <p class="empty-cart">Seu carrinho está vazio.</p>
