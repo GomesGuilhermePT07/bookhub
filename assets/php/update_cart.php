e2319ea8620a5ff51e7fc227c4c2faea57f76365
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-06 09:14:04 +0000

    add

--- assets/php/update_cart.php
+++ assets/php/update_cart.php
@@ -3,11 +3,11 @@ session_start();
 require_once 'check_login.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $book_id = $_POST['book_id'];
+    $isbn = $_POST['isbn'];
     $quantity = (int)$_POST['quantity'];
 
-    if ($quantity > 0 && isset($_SESSION['cart'][$book_id])) {
-        $_SESSION['cart'][$book_id]['quantity'] = $quantity;
+    if ($quantity > 0 && isset($_SESSION['cart'][$isbn])) {
+        $_SESSION['cart'][$isbn]['quantity'] = $quantity;
     }
 
     header('Location: cart.php');
