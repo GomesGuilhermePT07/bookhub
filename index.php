27cbaa78f3943bf7f3891976d3b569d40944cacf
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-10 15:12:18 +0000

    add

--- index.php
+++ index.php
@@ -49,7 +49,12 @@ session_start();
         </nav>
         <?php if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true): ?>
             <a href="./logins/registo_com_validacao.php" class="btn-action-ref">Registar</a>
-            <a href="./logins/login.php" class="btn-action-ref1">Entrar</a>
+            <a href="./logins/login.php" class="btn-action-ref1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                </svg>
+            </a>
         <?php else: ?>
             <a href="./detalhes_conta.php" class="btn-action-ref">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
