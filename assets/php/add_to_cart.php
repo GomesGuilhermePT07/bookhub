d82f874f562c38d4771f4676dd1f8bc2e6e334d0
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-10 10:37:40 +0000

    var_dump();

--- assets/php/add_to_cart.php
+++ assets/php/add_to_cart.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 // require_once 'check_login.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+var_dump($SESSION);
 require_once 'config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
