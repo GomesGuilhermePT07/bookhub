986bc0b1c0e8bc2d3510370184382ccee6b058cf
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-26 16:09:42 +0000

    add

--- gerir-requisicoes.php
+++ gerir-requisicoes.php
@@ -109,113 +109,14 @@ if (isset($_SESSION['id'])) {
             </a>
         <?php endif; ?>
     </header>
-
+    
     <main>
-        <!-- <h2>ISTO É O MAIN</h2> -->
-        <section class="first-section">
-            
-            <script src="../ModuloProjeto/assets/js/modal_livros.js"></script>
-            <script src="../ModuloProjeto/assets/js/carregar_livros.js"></script>
-            <script src="../ModuloProjeto/assets/js/remover_livros.js"></script>
-            <!-- <p>esta é a parte dos livros</p> -->
-        </section>
-
-        <section class="second-section">
-            <!-- <p>esta é a parte </p> -->
-        </section>
-
-        <section class="third-section">
-            <!-- <p>esta é a parte </p> -->
-        </section>
+        <p>Aqui é onde vão ficar todas as requisições dos utilizadores para serem geridas.</p>
+        <sub>Loading...</sub>
     </main>
 
-    <div class="slider">
-        <div class="slides">
-            <div class="slide">
-                <img src="../ModuloProjeto/assets/img/imagem_teste_slider1.png" class="slide" alt="Slide 1">
-                <div class="caption">Legenda do Slide 1</div>
-            </div>
-            <div class="slide">
-                <img src="../ModuloProjeto/assets/img/imagem_teste_slider2.png" class="slide" alt="Slide 2">
-                <div class="caption">Legenda do Slide 2</div>
-            </div>
-            <div class="slide">
-                <img src="../ModuloProjeto/assets/img/imagem_teste_slider3.png" class="slide" alt="Slide 3">
-                <div class="caption">Legenda do Slide 3</div>
-            </div>
-        </div> <!-- slides -->
-
-        <button class="prev">&#10094;</button>
-        <button class="next">&#10095;</button>
-    </div> <!-- slider -->
-    
-    <div class="ultimo-lancamento">
-        <p>Últimos livros adicionados:</p>
-    </div>
-
-    <button id="openModal" class="add-books">Adicionar livro</button>
-        <form action="./assets/php/captar_livro.php" method="POST" id="bookForm">
-        <dialog class="modal">
-            <h2>Adicionar Livro</h2>
-            <div class="modal-content">
-                <div class="modal-left"> <!--Coluna da esquerda-->
-                    <div class="book-image-container">
-                        <img id="bookImage" src="https://via.placeholder.com/128x186" alt="Imagem do Livro">
-                    </div>
-                    <div class="book-summary">
-                        <label for="summary" class="summary-label">Resumo</label>
-                        <textarea id="summary" name="summary" rows="4" placeholder="Resumo..." required></textarea>
-                        <button id="viewFullText" class="view-icon-btn" title="Ver resumo completo">
-                            <i class="fas fa-eye"></i>
-                        </button>
-                    </div>
-                    <dialog id="textModal" class="text-modal">
-                        <h2>Resumo completo do livro</h2>
-                        <p id="fullTextContent"></p>
-                        <button id="closeTextModal" class="modal-close-btn">Fechar</button>
-                    </dialog>
-                </div>
-                <div class="modal-right">
-                    <div class="inputUser">
-                        <input type="text" id="isbn" name="isbn" class="modal-input" required>
-                        <label for="isbn" class="labelInput">Código ISBN</label>
-                    </div>
-                
-                    <div class="inputUser">
-                        <input type="text" id="title" name="title" class="modal-input" required>
-                        <label for="title" class="labelInput">Título do Livro</label>
-                    </div>
-                
-                    <div class="inputUser">
-                        <input type="text" id="edition" name="edition" class="modal-input" required>
-                        <label for="edition" class="labelInput">Edição</label>
-                    </div>
-                
-                    <div class="inputUser">
-                        <input type="text" id="author" name="author" class="modal-input" required>
-                        <label for="author" class="labelInput">Autor</label>
-                    </div>
-                
-                    <div class="inputUser">
-                        <input type="text" id="numero_paginas" name="numero_paginas" class="modal-input" required>
-                        <label for="numero_paginas" class="labelInput">Nº de páginas</label>
-                    </div>
-
-                    <div class="inputUser">
-                        <input type="number" id="quantity" name="quantity" class="modal-input" min="1" value="1" required>
-                        <label for="quantity" class="labelInput">Quantidade</label>
-                    </div>
-
-                    <div class="modal-buttons-container">
-                        <button type="submit" name="submit" id="saveBook" class="modal-buttons">Guardar livro</button>
-                        <button type="button" name="submit" id="closeModal" class="modal-buttons1">Fechar</button>
-                    </div>
-                </div>
-            </div>                
-        </dialog>
-        </form>
     <footer>
-        <!-- <p>&copy; 2025 BOOKhub. Todos os direitos reservados.</p> -->
+        <p>&copy; 2025 BOOKhub. Todos os direitos reservados.</p>
     </footer>
 </body>
 </html>
