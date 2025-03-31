e45fa9b522779c10094e8388614561b0b80fecd1
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-03-31 09:51:54 +0100

    add

--- assets/js/carregar_livros.js
+++ assets/js/carregar_livros.js
@@ -41,8 +41,13 @@ document.addEventListener("DOMContentLoaded", function () {
                             <img src="${coverUrl}" alt="Capa do livro ${book.titulo}" class="book-cover">
                             <h5>${book.titulo}</h5>
                             <p><b>Autor: </b>${book.autor}</p>
+                            
                             <button class="add-to-cart">Adicionar ao carrinho</button>
-                            <button class="remove-book">Remover</button>
+                            
+                            <form action="../ModuloProjeto/assets/php/remover_livro.php">
+                                <button class="remove-book">Remover</button>
+                            </form>
+
                         </div>`;
                     bookListContainer.innerHTML += bookHtml;
                 }
