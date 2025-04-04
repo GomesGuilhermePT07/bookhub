f31048b388960444f3ed41bd9392ae359b49400b
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-04 09:21:41 +0100

    add

--- assets/php/config.php
+++ assets/php/config.php
@@ -6,9 +6,16 @@ $dbname = "bookhubjb";
 
 
 // Criar conexão
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+// $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+try{
+    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname", $dbusername, $dbpassword);
+    echo "Conectado com sucesso";
+} catch (PDOException $e) {
+    die("Erro ao conectar à base de dados: " . $e->getMessage());
+}
 
 // Checar conexão
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
