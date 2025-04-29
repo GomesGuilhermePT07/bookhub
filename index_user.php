a45e3dcfd81ed975e3064dde07b3b9686b162a85
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-29 14:30:31 +0100

    2 images off the slider

--- index_user.php
+++ index_user.php
@@ -141,14 +141,14 @@ if (isset($_SESSION['id'])) {
                 <img src="/ModuloProjeto/assets/img/Banner_Bookhub.jpg" alt="Slide 1">
                 <div class="caption">Legenda do Slide 1</div>
             </div>
-            <div class="slide">
+            <!-- <div class="slide">
                 <img src="./assets/img/imagem_teste_slider2.png" alt="Slide 2">
                 <div class="caption">Legenda do Slide 2</div>
             </div>
             <div class="slide">
                 <img src="./assets/img/imagem_teste_slider3.png" alt="Slide 3">
                 <div class="caption">Legenda do Slide 3</div>
-            </div>
+            </div> COMENTADO TEMPORARIAMENTE -->
         </div>
         <button class="prev">&#10094;</button>
         <button class="next">&#10095;</button>
