eb25e75feb4a73409374ae53e9f7b3a122385eba
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-05-15 12:07:45 +0100

    checking if the logged person is user or admin

--- assets/php/check_login.php
+++ assets/php/check_login.php
@@ -1,8 +1,16 @@
 <?php
 session_start();
+require_once 'config.php';
 
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_SESSION['id'])) {
     header("Location: /ModuloProjeto/logins/login.php");
     exit;
 }
+
+// Verificar se é admin
+$stmt = $pdo->prepare("SELECT admin FROM utilizadores WHERE id = ?;");
+$stmt->execute([$_SESSION['id']]);
+$user = $stmt->fetch();
+
+$_SESSION['admin'] = (isset($user['admin'])) ? $user['admin'] : 0;
 ?>
