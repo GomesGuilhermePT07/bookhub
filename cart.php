da019b3120ed2ec5944a9df594f83e64b1a96f1a
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-05-29 11:40:13 +0100

    commit

--- cart.php
+++ cart.php
@@ -3,8 +3,8 @@
     require_once 'assets/php/config.php';
     require_once 'assets/php/check_login.php';
     var_dump($loggedIn); 
-var_dump($cartItems); 
-var_dump($_SESSION);
+    var_dump($cartItems); 
+    var_dump($_SESSION);
 
     $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
     if ($conn->connect_error) {
