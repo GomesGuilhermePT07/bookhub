4029d843648c484b0bc2c8059053999dac22acfe
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-06-17 12:01:00 +0100

    add

--- assets/php/notificar_requisicao.php
+++ assets/php/notificar_requisicao.php
@@ -151,17 +151,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </style>
 </head>
 <body>
-
-<header>
-
+    <header>
         <div class="box-img-header">
             <?php if ($_SESSION['admin'] == 1): ?>
                 <a href="index.php">
-                    <img class="img-logo" src="../img/bookhubFavicon.png" height="80" width="80" alt="Logo BOOKhub">
+                    <img class="img-logo" src="assets/img/bookhubFavicon.png" height="80" width="80" alt="Logo BOOKhub">
                 </a>
             <?php elseif ($_SESSION['admin'] == 0): ?>
                 <a href="index_user.php">
-                    <img class="img-logo" src="../img/bookhubFavicon.png" height="80" width="80" alt="Logo BOOKhub">
+                    <img class="img-logo" src="assets/img/bookhubFavicon.png" height="80" width="80" alt="Logo BOOKhub">
                 </a>
             <?php endif; ?>
         </div>
@@ -188,21 +186,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </svg>
             </a>
 
-            <?php if ($_SESSION['admin'] == 0): ?>
+            <!-- <?php if ($_SESSION['admin'] == 0): ?>
                 <a href="./wishlist.php" class="nav-links">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-suit-heart" viewBox="0 0 16 16">
                         <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.6 7.6 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
                     </svg>
                 </a>
-            <?php endif; ?>
+            <?php endif; ?> -->
 
-            <?php if ($_SESSION['admin'] == 1): ?>
+            <!-- <?php if ($_SESSION['admin'] == 1): ?>
                 <a href="./estatisticas.php" class="nav-links">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-percent" viewBox="0 0 16 16">
                         <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0M4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5m7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
                     </svg>
                 </a>
-            <?php endif; ?>
+            <?php endif; ?> -->
 
             <?php if ($_SESSION['admin'] == 0): ?>
                 <a href="./cart.php" class="nav-links">
@@ -254,9 +252,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </svg>
             </a>
         <?php endif; ?>
-
     </header>
-
+    
     <div class="container">
         <h1>Notificar Utilizador</h1>
         
