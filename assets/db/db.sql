2817c76f5c28f70acd2f63d62e280e376ae6b9bf
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-06-25 09:01:44 +0100

    add

--- assets/db/db.sql
+++ assets/db/db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE bookhubjb;
 USE bookhubjb;
 
 CREATE TABLE utilizadores(
-    id INT(11) PRIMARY KEY AUTO_INCREMENT
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome_completo VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -20,3 +20,13 @@ CREATE TABLE livros(
     quantidade INT NOT NULL,
     resumo TEXT
 );
+
+CREATE TABLE carrinho(
+    id_utilizador INT(11) NOT NULL,
+    cod_isbn VARCHAR(15) NOT NULL,
+    quantidade INT NOT NULL DEFAULT 1,
+    PRIMARY KEY (id_utilizador, cod_isbn),
+    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id),
+    FOREIGN KEY (cod_isbn) REFERENCES livros(cod_isbn)
+);
+
