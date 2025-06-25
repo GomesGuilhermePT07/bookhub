3c510c2e0a7538cc03b887bfdd4972cf2e6f71f3
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-06-25 14:21:54 +0100

    add

--- logins/reset_password.php
+++ logins/reset_password.php
@@ -58,20 +58,25 @@ try {
     <link rel="stylesheet" href="../assets/css/login_style.css">
 </head>
 <body>
-    <div class="login-container">
-        <h1>Definir Nova Password</h1>
+    <div class="glow"></div> <!-- Adicionar este elemento para o efeito de fundo -->
+    
+    <div class="password-reset-container"> <!-- Alterar classe -->
+        <h1 class="password-reset-header">Definir Nova Password</h1> <!-- Adicionar classe -->
+        
         <?php if ($error): ?>
-            <div class="error"><?= $error ?></div>
+            <div class="password-message password-error"><?= $error ?></div> <!-- Alterar classes -->
         <?php elseif ($success): ?>
-            <div class="success"><?= $success ?></div>
+            <div class="password-message password-success"><?= $success ?></div> <!-- Alterar classes -->
         <?php else: ?>
-            <form method="POST">
-            <input type="hidden" name="token" value="<?php echo htmlspecialchars(isset($_GET['token']) ? $_GET['token'] : '', ENT_QUOTES, 'UTF-8'); ?>">
-                <div class="input-container">
+            <form method="POST" class="password-reset-form"> <!-- Adicionar classe -->
+                <input type="hidden" name="token" value="<?php echo htmlspecialchars(isset($_GET['token']) ? $_GET['token'] : '', ENT_QUOTES, 'UTF-8'); ?>">
+                
+                <div class="password-input-group"> <!-- Alterar classe -->
                     <i class="fa fa-lock"></i>
                     <input type="password" name="nova_password" placeholder="Nova password" required minlength="8">
                 </div>
-                <button type="submit">Alterar Password</button>
+                
+                <button type="submit" class="password-reset-btn">Alterar Password</button> <!-- Adicionar classe -->
             </form>
         <?php endif; ?>
     </div>
