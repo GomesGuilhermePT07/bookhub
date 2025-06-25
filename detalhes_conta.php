463edfec9fcea683f4f63de5ba81659ec0a11000
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-06-25 11:46:26 +0100

    error fixed: toggle password visibility wasn't working. Solution: delete the password part

--- detalhes_conta.php
+++ detalhes_conta.php
@@ -187,14 +187,14 @@ $cartCount = 0;
         </div>
 
         <!-- Password -->
-        <div class="detail-card">
+        <!-- <div class="detail-card">
             <div class="detail-header">
                 <div class="detail-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
                         <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
                     </svg>
                 </div>
-                <!-- <div class="password-field">
+                <div class="password-field">
                     <div>
                         <div class="detail-title">Password</div>
                         <div class="detail-content">
@@ -211,9 +211,9 @@ $cartCount = 0;
                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8"/>
                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
                     </svg>
-                </div> -->
+                </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Gênero -->
         <div class="detail-card">
